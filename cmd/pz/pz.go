58d4ab431904ab93734a8e90723fbef0946ef63c
NikhilX <nikhil@pubx.ai>
2024-10-26 14:13:28 +0530

    added block-ip and unblock-ip commands, added post req for bunnyClient

--- cmd/pz/pz.go
+++ cmd/pz/pz.go
@@ -1,23 +1,26 @@
 package pz
 
 import (
-    "github.com/NikhilMJagtap/bunny-cli/client"
-    pzGetCmd "github.com/NikhilMJagtap/bunny-cli/cmd/pz/get"
-    pzListCmd "github.com/NikhilMJagtap/bunny-cli/cmd/pz/list"
-    "github.com/spf13/cobra"
+	"github.com/NikhilMJagtap/bunny-cli/client"
+	pzBlockIPCmd "github.com/NikhilMJagtap/bunny-cli/cmd/pz/block-ip"
+	pzGetCmd "github.com/NikhilMJagtap/bunny-cli/cmd/pz/get"
+	pzListCmd "github.com/NikhilMJagtap/bunny-cli/cmd/pz/list"
+	"github.com/spf13/cobra"
 )
 
 var pzCommand *cobra.Command = nil
 
 func GetPZCommand(bunnyClient *client.BunnyClient) *cobra.Command {
-    if pzCommand == nil {
-        pzCommand = &cobra.Command{
-            Use:   "pz <command> [flags]",
-            Short: "Manage Pull Zones for BunnyCDN",
-            Long:  "",
-        }
-    }
-    pzCommand.AddCommand(pzListCmd.GetListCommand(bunnyClient))
-    pzCommand.AddCommand(pzGetCmd.GetGetPZCommand(bunnyClient))
-    return pzCommand
+	if pzCommand == nil {
+		pzCommand = &cobra.Command{
+			Use:   "pz <command> [flags]",
+			Short: "Manage Pull Zones for BunnyCDN",
+			Long:  "",
+		}
+	}
+	pzCommand.AddCommand(pzListCmd.GetListCommand(bunnyClient))
+	pzCommand.AddCommand(pzGetCmd.GetGetPZCommand(bunnyClient))
+	pzCommand.AddCommand(pzBlockIPCmd.GetBlockIPCommand(bunnyClient))
+	pzCommand.AddCommand(pzBlockIPCmd.GetUnblockIPCommand(bunnyClient))
+	return pzCommand
 }
